8a66b5c840b9f3054301bbfe220ff6868e0bcad7
karthika subramani <karthikaa08@gmail.com>
2019-04-23 16:32:37 -0400

    Update docker_monitor.py

--- docker_monitor.py
+++ docker_monitor.py
@@ -3,7 +3,6 @@ from docker_config import *
 import os
 import time
 import datetime
-import tarfile
 
 client = docker.from_env()
 
@@ -13,38 +12,38 @@ def get_time():
 
 def initiate_container(url, id, script_name, iteration_count,  container_timeout):	
 
-	## create and setup container ##
+    ## create and setup container ##
     print(get_time() + 'container_'+id+' creating!!')
-    container_id  = client.containers.run(image=docker_image,name='container_'+id,volumes = vols,
-										  shm_size='1G', user=docker_user, 
-										  publish_all_ports=True,stdout=True, stderr=True, detach=True, cpu_quota=300000, cpu_period=100000)
-    print(get_time() + 'container_'+id+' created successfully!!')
-	
+    container_id  = client.containers.create(image=docker_image,name='container_'+id,volumes = vols,
+                                            shm_size='1G', user=docker_user, 
+                                            publish_all_ports=True, detach=False)
+    container = client.containers.get('container_'+str(id))
+    container.start()
+    print(get_time() + 'container_'+id+' created successfully!!')    
+    
     ## wait for display to be activated ##
     time.sleep(10)
-    return execute_script(url, id, script_name,  iteration_count, container_timeout)
+    execute_script(url, id, script_name,  iteration_count, container_timeout-100)
 
 def execute_script(url, id, script_name,  iteration_count, container_timeout):	
-	## Execute javascript file
-	print(get_time() +'container_'+id+': Executing javascript')
-	container = client.containers.get('container_'+str(id))
-		
-	logs = container.exec_run(cmd=['node',script_name,url,id,str(iteration_count)], user=docker_user,stdout=True, stderr=True, detach=True)	
-	time.sleep(container_timeout-100)
-	print(get_time() +'container_'+id+': Execution complete!!')	
-	return export_log(id)
+    ## Execute javascript file
+    print(get_time() +'container_'+id+': Executing javascript')
+    container = client.containers.get('container_'+str(id))
+    #logs = container.attach(stream=True,stdout=True,stderr=True)
+    _,logs = container.exec_run(cmd=['node',script_name,url,id,str(iteration_count),str(container_timeout)], user=docker_user, detach=False, stream=True)
+    time.sleep(container_timeout-30)
+    for log in logs:
+        print(log)
+    print(get_time() +'container_'+id+': Execution complete!!')	
 
 def stop_container(id):
-	try:
-		container = client.containers.get('container_'+str(id))
-		if container:
-			print(get_time() + 'container_'+id+' stopping!!')
-			container.pause()
-			container.stop()
-		
-	except Exception as e:
-		return -1
-		
+    container = client.containers.get('container_'+str(id))
+    if container:
+        print(get_time() + 'container_'+id+' stopping!!')
+        container.pause()
+        time.sleep(2)
+        container.stop()
+
 def remove_containers():
     while client.containers.list():		
         for c in client.containers.list():
@@ -58,7 +57,7 @@ def resume_container(url, id, script_name, iteration_count, container_timeout):
         container.start()
         ## wait for display to be activated ##
         time.sleep(10)
-        execute_script(url,id, script_name, iteration_count, container_timeout)
+        execute_script('about:blank',id, script_name, iteration_count, container_timeout-100)
 
 def export_container(id, count):
     container = client.containers.get('container_'+str(id))
@@ -85,35 +84,23 @@ def export_container(id, count):
         bits, stat = container.get_archive('/home/pptruser/chromium/chrome_debug.log')
         for chunk in bits:
             f.write(chunk)
+    return check_if_success(id,count)
 
-def export_log(id):
-	container = client.containers.get('container_'+str(id))
-	print(get_time() + 'container_'+id+' exporting files!!')
-	dir_path = './permission_results/container_'+id+'/'
-	if not os.path.exists(dir_path):
-		os.makedirs(dir_path)
-	with open(dir_path+'chrome_log.tar', 'w') as f:
-		bits, stat = container.get_archive('/home/pptruser/chromium/chrome_debug.log')
-		for chunk in bits:
-	    		f.write(chunk)
-	with open(dir_path+'logs.tar', 'w') as f:
-		bits, stat = container.get_archive('/home/pptruser/logs/')
-		for chunk in bits:
-	    		f.write(chunk)
-	log_tar_dir = 'permission_results/container_'+id+'/logs.tar'
-	t = tarfile.open(log_tar_dir,'r')
-	log_name = 'logs/'+id+'_sw.log'
-	res=-99
-	err=-1
-	if log_name in t.getnames():
-		f = t.extractfile(log_name)
-		data = f.read()
-		res = data.find('Page Load Complete')
-		err = data.find('Chromium Crashed')
-	if err>-1:
-		return -99
-	stop_container(id)
-	return res
+def check_if_success(id,count):
+    import tarfile
+    print(get_time() + 'container_'+id+' checking status!!')
+    log_tar_dir = 'results/container_'+id+'/'+str(count)+'/logs.tar'
+    t = tarfile.open(log_tar_dir,'r')
+    log_name = 'logs/'+id+'_sw.log'
+    res=-99
+    if log_name in t.getnames():
+        f = t.extractfile(log_name)
+        data = f.read()
+        res = data.find('Visiting Page ended')
+    if res>-1:
+        return True
+    return False
+		
 
 def docker_prune():
     ## Remove containers that are unused  ##
@@ -124,13 +111,20 @@ def docker_prune():
 
 
 def test():
+    '''
     remove_containers()
-    initiate_container('https://blacktv.tk/','201', 'capture_permission_requests.js',0, 200 )    
-    #stop_container('201')
-    #export_log('201','0')
+    initiate_container('https://evangelistjoshuaforum.com/','tes_100', 'capture_notifications.js','0', 330 )    
+    count=0
+    while count<2:
+        stop_container('tes_100')
+        export_container('tes_100',str(count-1))
+        time.sleep(300)
+        resume_container('https://evangelistjoshuaforum.com/','tes_100','capture_notifications.js',count,330)
+        count=count+1
     '''
-    time.sleep(10)
-    resume_container('https://blacktv.tk/','201','capture_notifications.js',0,600)
-    '''
+    export_container('833','9')
+    #print(check_if_success('833','0'))
+   
+    
 if __name__== "__main__":
     test()
