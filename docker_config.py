3eeb0cacc2e899b1d001eaa57a7b449f4cc7278d
karthika subramani <karthikaa08@gmail.com>
2019-04-23 16:33:56 -0400

    Update docker_config.py

--- docker_config.py
+++ docker_config.py
@@ -7,9 +7,9 @@ docker_image = 'dockerammu/docker_puppeteer_chromium_xvfb:ver1'
 docker_user = 'pptruser'
 docker_container_home = '/home/pptruser/'
 docker_shared_dir_root = project_dir
-vols = { docker_shared_dir_root + '/app'              :{'bind':docker_container_home + 'app','mode':'rw'}, 
-	docker_shared_dir_root + '/logs'          :{'bind':docker_container_home + 'logs','mode':'rw'}}
-''',
+vols = { docker_shared_dir_root + '/app'              :{'bind':docker_container_home + 'app','mode':'rw'}}
+''', 
+	docker_shared_dir_root + '/logs'          :{'bind':docker_container_home + 'logs','mode':'rw'},
 	        docker_shared_dir_root + '/screenshots'   :{'bind':docker_container_home + 'screenshots','mode':'rw'},
             docker_shared_dir_root + '/resources'     :{'bind':docker_container_home + 'resources','mode':'rw'},
 	       
@@ -18,7 +18,7 @@ vols = { docker_shared_dir_root + '/app'              :{'bind':docker_container_
 permission_collection_script   = 'capture_permission_requests.js'
 notification_collection_script = 'capture_notifications.js'
 
-CRAWL_MAX_CONTAINERS = 50
-CRAWL_TIMEOUT = 340
-ANALYSIS_MAX_CONTAINERS = 3
-ANALYSIS_TIMEOUT = 930
+CRAWL_MAX_CONTAINERS = 10
+CRAWL_TIMEOUT = 180
+ANALYSIS_MAX_CONTAINERS = 5
+ANALYSIS_TIMEOUT = 1000
