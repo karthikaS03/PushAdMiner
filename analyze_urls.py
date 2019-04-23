581097c28a2be9bb2c373eaffe086c815879aeee
karthika subramani <karthikaa08@gmail.com>
2019-04-23 16:32:06 -0400

    Update analyze_urls.py

--- analyze_urls.py
+++ analyze_urls.py
@@ -4,59 +4,55 @@ import concurrent.futures
 import docker
 import time
 import datetime
-from docker_monitor import *
 import os
+
 from docker_config import *
+from docker_monitor import *
+from api_calls import api_requests
+
 
 client = docker.from_env()
-
-def get_time():
-	currentDT = datetime.datetime.now()
-	return '['+currentDT.strftime("%Y-%m-%d %H:%M:%S") +'] '
 
 def process_urls_parallel(analysis_urls, script_file, container_timeout, max_containers, visit_count):
 	futures={}
 	count=0
-	processed_url_ids = []
+	processed_url_ids = set()
 	urls = analysis_urls.copy()
-	with concurrent.futures.ThreadPoolExecutor(max_workers = max_containers) as executor:
+	with concurrent.futures.ProcessPoolExecutor(max_workers = max_containers) as executor:
 		while len(urls)>0:
 			## Submit jobs to container ##
 			for i in range(min(len(urls),max_containers)):
 				id = urls.keys()[0]
 				url = urls.pop(id)
 				if visit_count==0:
-					futures[executor.submit(initiate_container, url, str(id), script_file, container_timeout)] = str(id)		
+					futures[executor.submit(initiate_container, url, str(id), script_file, visit_count, container_timeout)] = str(id)		
 				else:
-					futures[executor.submit(resume_container,url, str(id), script_file, container_timeout)] = str(id)	
-				
+					futures[executor.submit(resume_container,url, str(id), script_file, visit_count, container_timeout)] = str(id)	
+			
+			time.sleep(container_timeout)
 			res_futures = concurrent.futures.wait(futures, timeout=container_timeout, return_when= concurrent.futures.ALL_COMPLETED)
 			
 			for future in res_futures[0]:
 				id = futures.pop(future)				
 				try:
-					log = future.result(timeout=container_timeout)
+					res = future.result(timeout=container_timeout)
 					print (get_time() + 'Container_'+ str(id) +': Completed successfully!!'	)
-					processed_url_ids.append(id)		
+						
 				except Exception as exc:
 					print(get_time() +  'Container_' + str(id) +': Exception ')
-					print(exc)					
-				export_container(id, visit_count)
-				stop_container(id)
+					print(exc)			
+				stop_container(id)		
+				res = export_container(id, visit_count)	
+				if res:
+					processed_url_ids.add(id)	
 			for future in res_futures[1]:
 				id =  futures.pop(future)						
 				print(get_time() +  'Container_' + str(id) +': Timeout occured!!')	
-				export_container(id, visit_count)	
-				stop_container(id)								
-			
+				stop_container(id)		
+				res = export_container(id, visit_count)	
+				if res:
+					processed_url_ids.add(id)					
 	return processed_url_ids
-
-
-'''
-df = pd.read_csv('top-1m-filtered.csv',skiprows=range(1,1000),nrows=10)
-print df.columns
-'''
-
 
 def stop_running_containers():
 	while client.containers.list():		
@@ -66,41 +62,50 @@ def stop_running_containers():
 			c.remove()
 
 def fetch_urls_with_notifications():
-	urls = [
-		'https://serialifilmi.com/',            
-		'https://mydramaoppa.com/',  
-		'https://www.tubebg.com/',
-		'https://shippuden.tv/',
-		'https://igg-games.com/',
-		'https://www.okanime.com/',
-		'https://www.2giga.link/',
-		'https://blacktv.tk/',
-		'https://kinogo2019.com/',
-		'https://www.nopeporno.com/',
-		'https://aserialov.net/']
-	start_id = 9500
-	urls = ['https://shippuden.tv/', 'https://beneguy.com', 'https://blacktv.tk' ]
-	notification_urls = {start_id+i:url for i,url in enumerate(urls) }
-	return notification_urls
-
+	results = api_requests.fetch_urls_api(200,'true','true')
+	crawl_urls={}
+	for item in results:
+		id = item[0]
+		url = item[1]
+		crawl_urls[id]=url
+		api_requests.update_url_api(id,'visit_status','4')
+		
+	return crawl_urls
 
 def process_urls_with_notifications():	
 	count=0
-	while count<5:
-		notification_urls  =  fetch_urls_with_notifications()
-		processed_urls = process_urls_parallel(notification_urls, notification_collection_script, ANALYSIS_TIMEOUT, ANALYSIS_MAX_CONTAINERS, count)
-		print(processed_urls)
+	notification_urls  =  fetch_urls_with_notifications()
+	'''
+	notification_urls={953:'https://goalhighlight.site/',
+	475:'https://kingofgym.com/',
+	833:'https://thekingwarehouse.com/',
+	955:'https://vuivuitv.site/',
+	599:'https://vuilen.info/',
+	179:'https://kingelia.com/',
+	701:'https://hotvideo.top/',
+	978:'https://tipsbook.site/',
+	1039:'https://xemhub.net/',
+	1008:'https://evangelistjoshuaforum.com/'
+	}
+	'''
+	while count<10:				
+		processed_ids = process_urls_parallel(notification_urls, notification_collection_script, ANALYSIS_TIMEOUT, ANALYSIS_MAX_CONTAINERS, count)
+		print(processed_ids)
+		for id in processed_ids:
+			api_requests.update_url_api(id,'is_analyzed_desktop','true')
 		count +=1
-		time.sleep(1200)
+		time.sleep(1800)
+		
 	
 
 def main():	
-	stop_running_containers()
-	'''
-	prune unused removed containers
-	'''
-	#docker_prune()
-	process_urls_with_notifications()
+	while True:
+		stop_running_containers()
+		'''
+		prune unused removed containers
+		'''
+		docker_prune()
+		process_urls_with_notifications()
 
 
 
