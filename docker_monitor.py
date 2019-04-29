9b8bfef2758ac56263969f07d645df956112d143
karthika subramani <karthikaa08@gmail.com>
2019-04-29 14:36:39 -0400

    updated logging and success check

--- docker_monitor.py
+++ docker_monitor.py
@@ -1,45 +1,54 @@
 import docker
-from docker_config import *
 import os
 import time
 import datetime
+import logging
+
+from docker_config import *
+from api_calls import api_requests
 
 client = docker.from_env()
+
+
+logging.basicConfig(filename='output2.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
 
 def get_time():
 	currentDT = datetime.datetime.now()
 	return '['+currentDT.strftime("%Y-%m-%d %H:%M:%S") +'] '
 
 def initiate_container(url, id, script_name, iteration_count,  container_timeout):	
-
-    ## create and setup container ##
-    print(get_time() + 'container_'+id+' creating!!')
-    container_id  = client.containers.create(image=docker_image,name='container_'+id,volumes = vols,
-                                            shm_size='1G', user=docker_user, 
-                                            publish_all_ports=True, detach=False)
-    container = client.containers.get('container_'+str(id))
-    container.start()
-    print(get_time() + 'container_'+id+' created successfully!!')    
-    
-    ## wait for display to be activated ##
-    time.sleep(10)
-    execute_script(url, id, script_name,  iteration_count, container_timeout-100)
+    try:
+        ## create and setup container ##
+        logging.info(get_time() + 'container_'+id+' creating!!')
+        container_id  = client.containers.create(image=docker_image,name='container_'+id,volumes = vols,
+                                                shm_size='1G', user=docker_user, 
+                                                publish_all_ports=True, detach=False)
+        container = client.containers.get('container_'+str(id))
+        container.start()
+        logging.info(get_time() + 'container_'+id+' created successfully!!')    
+        api_requests.update_url_api(id,'visit_status','4')
+        ## wait for display to be activated ##
+        time.sleep(15)
+        execute_script(url, id, script_name,  iteration_count, container_timeout-100)
+    except Exception as e:
+        print(e) 
 
 def execute_script(url, id, script_name,  iteration_count, container_timeout):	
     ## Execute javascript file
-    print(get_time() +'container_'+id+': Executing javascript')
+    logging.info(get_time() +'container_'+id+': Executing javascript')
     container = client.containers.get('container_'+str(id))
     #logs = container.attach(stream=True,stdout=True,stderr=True)
     _,logs = container.exec_run(cmd=['node',script_name,url,id,str(iteration_count),str(container_timeout)], user=docker_user, detach=False, stream=True)
     time.sleep(container_timeout-30)
     for log in logs:
-        print(log)
-    print(get_time() +'container_'+id+': Execution complete!!')	
+        logging.info('Container_'+id+'LOG :: '+log)
+    logging.info(get_time() +'container_'+id+': Execution complete!!')	
 
 def stop_container(id):
     container = client.containers.get('container_'+str(id))
     if container:
-        print(get_time() + 'container_'+id+' stopping!!')
+        logging.info(get_time() + 'container_'+id+' stopping!!')
         container.pause()
         time.sleep(2)
         container.stop()
@@ -53,7 +62,7 @@ def remove_containers():
 def resume_container(url, id, script_name, iteration_count, container_timeout):
     container = client.containers.get('container_'+str(id))
     if container:
-        print(get_time() + 'container_'+id+' resuming!!')
+        logging.info(get_time() + 'container_'+id+'_'+str(iteration_count)+' resuming!!')
         container.start()
         ## wait for display to be activated ##
         time.sleep(10)
@@ -61,7 +70,7 @@ def resume_container(url, id, script_name, iteration_count, container_timeout):
 
 def export_container(id, count):
     container = client.containers.get('container_'+str(id))
-    print(get_time() + 'container_'+id+' exporting files!!')
+    logging.info(get_time() + 'container_'+id+'_'+str(count)+' exporting files!!')
     dir_path = './results/container_'+id+'/'
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
@@ -80,6 +89,10 @@ def export_container(id, count):
         bits, stat = container.get_archive('/home/pptruser/resources/')
         for chunk in bits:
             f.write(chunk)
+    with open(dir_path+'dowanloads.tar', 'w') as f:
+        bits, stat = container.get_archive('/home/pptruser/Downloads/')
+        for chunk in bits:
+            f.write(chunk)
     with open(dir_path+'chrome_log.tar', 'w') as f:
         bits, stat = container.get_archive('/home/pptruser/chromium/chrome_debug.log')
         for chunk in bits:
@@ -88,7 +101,7 @@ def export_container(id, count):
 
 def check_if_success(id,count):
     import tarfile
-    print(get_time() + 'container_'+id+' checking status!!')
+    logging.info(get_time() + 'container_'+id+' checking status!!')
     log_tar_dir = 'results/container_'+id+'/'+str(count)+'/logs.tar'
     t = tarfile.open(log_tar_dir,'r')
     log_name = 'logs/'+id+'_sw.log'
@@ -96,7 +109,7 @@ def check_if_success(id,count):
     if log_name in t.getnames():
         f = t.extractfile(log_name)
         data = f.read()
-        res = data.find('Visiting Page ended')
+        res = data.find('Service Worker Registered')
     if res>-1:
         return True
     return False
@@ -107,7 +120,7 @@ def docker_prune():
     try:
         client.containers.prune()		
     except Exception as e:
-        print(e	)
+        logging.info(e	)
 
 
 def test():
@@ -122,8 +135,8 @@ def test():
         resume_container('https://evangelistjoshuaforum.com/','tes_100','capture_notifications.js',count,330)
         count=count+1
     '''
-    export_container('833','9')
-    #print(check_if_success('833','0'))
+    #export_container('833','9')
+    logging.info(check_if_success('1786','0'))
    
     
 if __name__== "__main__":
