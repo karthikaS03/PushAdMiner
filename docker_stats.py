1ced4108867d1cc73a74bde44831ff862a11381d
karthika subramani <karthikaa08@gmail.com>
2019-04-29 14:43:06 -0400

    Added multi-processing

--- docker_stats.py
+++ docker_stats.py
@@ -2,31 +2,67 @@
 import docker 
 import json
 import time
+import datetime
 import itertools
 import pprint
+import concurrent.futures
+import os
+
+from docker_config import *
+
 client = docker.from_env()
+active_containers={}
+stats_dir ='./stats/'
+completed_containers = {}
+def get_time():
+	currentDT = datetime.datetime.now()
+	return '['+currentDT.strftime("%Y-%m-%d %H:%M:%S") +'] '
 
 
-def collect_docker_stats():
+def collect_container_stats(c):
+    count=0
+    print (get_time() + c.name +': started recording!!'	)
+    if c.name not in active_containers:
+        active_containers[c.name]=count
+    else:
+        count=active_containers[c.name]+1
+        active_containers[c.name]=count
+    with open(stats_dir+c.name+'_'+str(count)+'.json','w') as  f:
+        f.write('[\n')        
+        
+        try:
+            stat_stream = c.stats(decode=True, stream=True)
+            for stat in stat_stream:
+                stats = {}
+                stats['name'] = stat['name']
+                stats['memory_stats'] = stat['memory_stats']
+                stats['cpu_stats'] = stat['cpu_stats']
+                stats['time'] = get_time()
+                if stat['cpu_stats']['cpu_usage']['total_usage']==0:
+                    break
+                json.dump(stats,f, indent = True)
+                f.write(',\n')		
+            completed_containers[c.name]=1
+        except Exception as e:
+            print('error')
+        f.write(']')
+    print (get_time() + c.name +': finished recording!!'	)
+    return ''
+
+def collect_stats(max_containers):
     with open('container_stats.json','w') as  f:
         f.write('[\n')
-        while True:	
-            for c in client.containers.list():
-                stats = {}
-                try:
-                    stat = c.stats( stream=False)	
-                    stats['name'] = stat['name']
-                    stats['memory_stats'] = stat['memory_stats']
-                    stats['cpu_stats'] = stat['cpu_stats']
-                    stats['time'] = stat['read']
-                    json.dump(stats,f, indent = True)
-                    f.write(',\n')		
-                except Exception as e:
-                    continue
-            time.sleep(60)        
-            if not client.containers.list():
-                break
-        f.write(']')
+        futures={}
+	with concurrent.futures.ThreadPoolExecutor(max_workers = 100) as executor:        
+	    while True:	
+            
+                for c in client.containers.list():
+                    if c.name not in active_containers or c.name in completed_containers:
+                        if c.name in completed_containers:
+                            completed_containers.pop(c.name)
+                        print (get_time() + c.name +': container found!!'	)
+                        futures[executor.submit(collect_container_stats, c)] = str(c.name)		
+                      
 
 def GetHumanReadable(size,precision=2):
     suffixes=['B','KB','MB','GB','TB']
@@ -68,4 +104,4 @@ def measure_docker_stats():
     print('Overall Max Memory Usage :: '+ GetHumanReadable(max_usage))
     print('Overall Max CPU Usage :: '+ str(max_cpu_usage))
     
-measure_docker_stats()
+collect_stats(ANALYSIS_MAX_CONTAINERS)
