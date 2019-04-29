638c8d66eb640d191975a585d4ae63b88287b99f
karthika subramani <karthikaa08@gmail.com>
2019-04-29 14:35:48 -0400

    Updated logging and futures execution

--- analyze_urls.py
+++ analyze_urls.py
@@ -10,12 +10,14 @@ from docker_config import *
 from docker_monitor import *
 from api_calls import api_requests
 
+import logging
+
+logging.basicConfig(filename='output2.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 client = docker.from_env()
 
-def process_urls_parallel(analysis_urls, script_file, container_timeout, max_containers, visit_count):
+def process_urls_parallel(analysis_urls, script_file, container_timeout, max_containers):
 	futures={}
-	count=0
 	processed_url_ids = set()
 	urls = analysis_urls.copy()
 	with concurrent.futures.ProcessPoolExecutor(max_workers = max_containers) as executor:
@@ -23,89 +25,124 @@ def process_urls_parallel(analysis_urls, script_file, container_timeout, max_con
 			## Submit jobs to container ##
 			for i in range(min(len(urls),max_containers)):
 				id = urls.keys()[0]
-				url = urls.pop(id)
+				itm = urls.pop(id)
+				url = itm['url']
+				visit_count = itm['count']
+				if i!=0 and i%5==0:
+					time.sleep(200)
 				if visit_count==0:
-					futures[executor.submit(initiate_container, url, str(id), script_file, visit_count, container_timeout)] = str(id)		
+					futures[executor.submit(initiate_container, url, str(id), script_file, visit_count, container_timeout)] = (str(id),visit_count)		
 				else:
-					futures[executor.submit(resume_container,url, str(id), script_file, visit_count, container_timeout)] = str(id)	
+					futures[executor.submit(resume_container,url, str(id), script_file, visit_count, container_timeout)] = (str(id), visit_count)	
 			
-			time.sleep(container_timeout)
-			res_futures = concurrent.futures.wait(futures, timeout=container_timeout, return_when= concurrent.futures.ALL_COMPLETED)
-			
-			for future in res_futures[0]:
-				id = futures.pop(future)				
-				try:
-					res = future.result(timeout=container_timeout)
-					print (get_time() + 'Container_'+ str(id) +': Completed successfully!!'	)
-						
-				except Exception as exc:
-					print(get_time() +  'Container_' + str(id) +': Exception ')
-					print(exc)			
+			#time.sleep(container_timeout)
+			#res_futures = concurrent.futures.wait(futures, timeout=container_timeout, return_when= concurrent.futures.ALL_COMPLETED)
+			try:
+				for future in  concurrent.futures.as_completed(futures, timeout=container_timeout):
+					id, v_count = futures.pop(future)				
+					try:
+						#res = future.result()
+						print (get_time() + 'Container_'+ str(id) +': Completed successfully!!'	)	
+					except concurrent.futures.TimeoutError as ex:
+						logging.info(get_time() +  'Container_' + str(id) +': Timeout occured!!')
+					except Exception as exc:
+						logging.info(get_time() +  'Container_' + str(id) +': Exception ')
+						logging.info(exc)			
+					stop_container(id)		
+					res = export_container(id, v_count)	
+					if res:
+						processed_url_ids.add(id)	
+			except Exception as e:
+				for future in futures.keys():
+					id = futures.pop(future)				
+					try:
+						#res = future.result()
+						print (get_time() + 'Container_'+ str(id) +': Completed successfully!!'	)	
+					except concurrent.futures.TimeoutError as ex:
+						logging.info(get_time() +  'Container_' + str(id) +': Timeout occured!!')
+					except Exception as exc:
+						logging.info(get_time() +  'Container_' + str(id) +': Exception ')
+						logging.info(exc)			
+					stop_container(id)		
+					res = export_container(id, v_count)	
+					if res:
+						processed_url_ids.add(id)	
+
+			'''
+			for future in res_futures[1]:
+				id =  futures.pop(future)						
+				logging.info(get_time() +  'Container_' + str(id) +': Timeout occured!!')	
 				stop_container(id)		
 				res = export_container(id, visit_count)	
 				if res:
 					processed_url_ids.add(id)	
-			for future in res_futures[1]:
-				id =  futures.pop(future)						
-				print(get_time() +  'Container_' + str(id) +': Timeout occured!!')	
-				stop_container(id)		
-				res = export_container(id, visit_count)	
-				if res:
-					processed_url_ids.add(id)					
+			'''				
 	return processed_url_ids
 
 def stop_running_containers():
-	while client.containers.list():		
-		for c in client.containers.list():
-			print (c)			
-			c.stop()
-			c.remove()
+	
+	for c in client.containers.list():
+		print (c)			
+		c.stop()
+		c.remove()
 
-def fetch_urls_with_notifications():
-	results = api_requests.fetch_urls_api(200,'true','true')
-	crawl_urls={}
-	for item in results:
-		id = item[0]
-		url = item[1]
-		crawl_urls[id]=url
-		api_requests.update_url_api(id,'visit_status','4')
-		
-	return crawl_urls
+def fetch_urls_with_notifications(count):
+	if count>0:
+		logging.info('Fetching URLS')
+		results = api_requests.fetch_urls_api(count,'true','true')
+		crawl_urls={}
+		for item in results:
+			id = item[0]
+			url = item[1]
+			crawl_urls[str(id)]={'url': url,'count':0}
+			
+		return crawl_urls
+	return {}
 
 def process_urls_with_notifications():	
-	count=0
-	notification_urls  =  fetch_urls_with_notifications()
+	URL_COUNT = 500
+	notification_urls  =  fetch_urls_with_notifications(URL_COUNT)
 	'''
-	notification_urls={953:'https://goalhighlight.site/',
-	475:'https://kingofgym.com/',
-	833:'https://thekingwarehouse.com/',
-	955:'https://vuivuitv.site/',
-	599:'https://vuilen.info/',
-	179:'https://kingelia.com/',
-	701:'https://hotvideo.top/',
-	978:'https://tipsbook.site/',
-	1039:'https://xemhub.net/',
-	1008:'https://evangelistjoshuaforum.com/'
+	notification_urls={'953':{'url':'https://goalhighlight.site/','count':0},
+	'475':{'url':'https://kingofgym.com/','count':0},
+	'833':{'url':'https://thekingwarehouse.com/','count':0},
+	'955':{'url':'https://vuivuitv.site/','count':0},
+	'599':{'url':'https://vuilen.info/','count':0},
+	'179':{'url':'https://kingelia.com/','count':0},
+	'701':{'url':'https://hotvideo.top/','count':0},
+	'978':{'url':'https://tipsbook.site/','count':0},
+	'1039':{'url':'https://xemhub.net/','count':0},
+	'1008':{'url':'https://evangelistjoshuaforum.com/','count':0}
 	}
 	'''
-	while count<10:				
-		processed_ids = process_urls_parallel(notification_urls, notification_collection_script, ANALYSIS_TIMEOUT, ANALYSIS_MAX_CONTAINERS, count)
-		print(processed_ids)
+	
+	while True:			
+		processed_ids = process_urls_parallel(notification_urls, notification_collection_script, ANALYSIS_TIMEOUT, ANALYSIS_MAX_CONTAINERS)
 		for id in processed_ids:
 			api_requests.update_url_api(id,'is_analyzed_desktop','true')
-		count +=1
+			api_requests.update_url_api(id,'visit_status','5')
+		logging.info(processed_ids)
+		notification_urls = {id:notification_urls[id] for id in processed_ids}
+		for key in notification_urls.keys():
+			itm = notification_urls[key]
+			if itm['count']==10:
+				notification_urls.pop(key)
+			else:
+				itm['count'] = itm['count']+1
+		logging.info(notification_urls)
+		notification_urls.update(fetch_urls_with_notifications(URL_COUNT-len(notification_urls)))
 		time.sleep(1800)
 		
 	
 
-def main():	
-	while True:
-		stop_running_containers()
-		'''
-		prune unused removed containers
-		'''
-		docker_prune()
-		process_urls_with_notifications()
+def main():
+	stop_running_containers()
+	'''
+	prune unused removed containers
+	'''
+	docker_prune()
+	logging.info('Started Processing')
+	process_urls_with_notifications()
 
 
 
