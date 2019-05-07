c897a003529fd0661985262f3563dc1976d04eea
karthika subramani <karthikaa08@gmail.com>
2019-05-06 20:47:46 -0400

    Create config.py

---
+++ database/config.py
@@ -0,0 +1,10 @@
+#Postgres
+db_host = 'localhost'
+db_name = 'malvert_db'
+db_user = 'malvert_db_admin'
+db_password = 'n0m0r3sp@m'
+
+
+#Google Safe Browsing API
+gsb_key = 'AIzaSyC39LDvnQYaJOXoMXGpk5smfDll4-XFHq8'
+gsb_db_path = 'gsb_v4.db'
