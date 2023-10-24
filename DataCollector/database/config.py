b7527b2057981c596510ba68975417f38814b3b2
karthika subramani <ks54471@uga.edu>
2023-10-24 15:46:27 -0700

    Updated config.py

--- DataCollector/database/config.py
+++ DataCollector/database/config.py
@@ -1,10 +1,12 @@
+## To be filled
+
 #Postgres
-db_host = '172.19.52.107'
-db_name = 'malvert_db_final'
-db_user = 'malvert_db_admin'
-db_password = 'n0m0r3sp@m'
+db_host = ''
+db_name = ''
+db_user = ''
+db_password = ''
 
 
 #Google Safe Browsing API
-gsb_key = 'AIzaSyC39LDvnQYaJOXoMXGpk5smfDll4-XFHq8'
+gsb_key = ''
 gsb_db_path = 'gsb_v4.db'
